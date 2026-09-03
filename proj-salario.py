341dabf2ad0ff6ceac153da947e8897aac2fcc7d
Pietro Reis <reisopietro220109@gmail.com>
2026-09-03 17:03:50 -0300

    Add files via upload

---
+++ proj-salario.py
@@ -0,0 +1,5 @@
+hora = float(input("Digite a quanto você ganha por hora: "))
+trabaio = float(input("Digite quantas horas trabalhadas por mês: "))
+salario = hora * trabaio 
+
+print(f"O seu sálario do mês é {salario}")
